716f1290ee6bda41278460b92a41408555c84929
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-07-17 19:00:48 -0700

    About page optimized for tablet

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,13 +8,15 @@ export default function About() {
     <div className="bg-[#EDF3F8]">
       <div>
         <NavBar />
-        <div className="text-center mx-6 mt-12">
-          <h1 className='text-[#36536B] font-display text-[32px] leading-9 mb-10'>
+        <div className="text-center mx-6 md:mx-10 mt-12 md:mt-[80px]">
+          <h1 className="text-[#36536B] font-display text-[32px] leading-9 mb-10 md:text-[48px] md:leading-[56px] md:mb-[54px]">
             We empower innovators by delivering access to the financial system
           </h1>
-          <div className='mb-12'>
-            <h4 className='text-[#36536B] font-display text-[24px] mb-4 leading-8'>Our Vision</h4>
-            <p className='text-[#6C8294] font-public text-[15px] leading-7'>
+          <div className="mb-12 md:mb-[64px] md:flex md:justify-center md:gab-[10px]">
+            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
+              Our Vision
+            </h4>
+            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
               Our main goal is to build beautiful consumer experiences along
               with developer-friendly infrastructure. The result is an
               intelligent tool that gives everyone the ability to create amazing
@@ -22,9 +24,11 @@ export default function About() {
               democratizing financial services through technology.
             </p>
           </div>
-          <div className='mb-[76px]'>
-            <h4 className='text-[#36536B] font-display text-[24px] mb-4 leading-8'>Our Business</h4>
-            <p className='text-[#6C8294] font-public text-[15px] leading-7'>
+          <div className="mb-[76px] md:mb-[92px] md:flex md:justify-center md:gab-[10px]">
+            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
+              Our Business
+            </h4>
+            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
               At the core of our platform is the technical infrastructure APIs
               that connect consumers. Our innovative product provides key
               insights for businesses and individuals, as well as robust
@@ -38,32 +42,60 @@ export default function About() {
             alt="team members"
             width={449}
             height={300}
+            className="md:hidden"
+          />
+          <Image
+            src="/assets/about/tablet/image-team-members.jpg"
+            alt="team members"
+            width={769}
+            height={267}
+            className="hidden md:block"
           />
         </div>
-        <div className='flex justify-center items-center mt-12'>
+        <div className="flex justify-center items-center mt-12 md:hidden">
           <div className="bg-[#36536B] w-[327px] h-[1px] mb-4 opacity-25"></div>
         </div>
-        <div className="text-center">
-          <div className='mb-8'>
-            <h5 className='text-[#36536B] text-[16px] font-public leading-7'>Team Members</h5>
-            <p className='text-[#BA4270] font-display text-[56px] leading-[72px]'>300+</p>
+        <div className="text-center md:flex md:justify-between md:items-end md:mx-10 md:my-[64px]">
+          <div className="mb-8 md:mb-0">
+            <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
+            <h5 className="text-[#36536B] text-[16px] font-public leading-7">
+              Team Members
+            </h5>
+            <p className="text-[#BA4270] font-display text-[56px] leading-[72px]">
+              300+
+            </p>
+            <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
           </div>
-          <div className='mb-8'>
-            <h5 className='text-[#36536B] text-[16px] font-public leading-7'>Offices in the US</h5>
-            <p className='text-[#BA4270] font-display text-[56px] leading-[72px]'>3</p>
+          <div className="mb-8 md:mb-0">
+            <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
+            <h5 className="text-[#36536B] text-[16px] font-public leading-7">
+              Offices in the US
+            </h5>
+            <p className="text-[#BA4270] font-display text-[56px] leading-[72px]">
+              3
+            </p>
+            <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
           </div>
           <div>
-            <h5 className='text-[#36536B] text-[16px] font-public leading-7'>Transactions analyzed</h5>
-            <p className='text-[#BA4270] font-display text-[56px] leading-[72px]'>10M+</p>
+            <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
+            <h5 className="text-[#36536B] text-[16px] font-public leading-7">
+              Transactions analyzed
+            </h5>
+            <p className="text-[#BA4270] font-display text-[56px] leading-[72px]">
+              10M+
+            </p>
+            <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
           </div>
         </div>
-        <div className='flex justify-center items-center'>
+        <div className="flex justify-center items-center md:hidden">
           <div className="bg-[#36536B] w-[327px] h-[1px] mt-4 mb-5 opacity-25"></div>
         </div>
         <div className="text-center px-6 pb-12">
-          <div className='mb-12'>
-            <h4 className='text-[#36536B] font-display text-[24px] mb-4 leading-8'>The Culture</h4>
-            <p className='text-[#6C8294] font-public text-[15px] leading-7'>
+          <div className="mb-12 md:mb-[64px] md:flex md:justify-center md:gab-[10px]">
+            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
+              The Culture
+            </h4>
+            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
               We strongly believe there&apos;s always an opportunity to learn
               from each other outside of day-to-day work, whether it&apos;s
               company-wide offsites, internal hackathons, or co-worker meetups.
@@ -71,9 +103,11 @@ export default function About() {
               no matter the job title.
             </p>
           </div>
-          <div>
-            <h4 className='text-[#36536B] font-display text-[24px] mb-4 leading-8'>The People</h4>
-            <p className='text-[#6C8294] font-public text-[15px] leading-7'>
+          <div className='md:mb-[64px] md:flex md:justify-center md:gab-[10px]'>
+            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
+              The People
+            </h4>
+            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
               We&apos;re all passionate about building a more efficient and
               inclusive financial infrastructure together. At PayAPI, we have
               diverse backgrounds and skills.
