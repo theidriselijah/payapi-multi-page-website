4ea020cdc0ba5828c4a81b7331d43c20731f86ae
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-07-19 13:44:51 -0700

    Project fully responsive

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,164 +8,167 @@ export default function Contact() {
     <div className="bg-[#EDF3F8]">
       <div>
         <NavBar />
-        <div className="px-6 pt-12 md:px-[100px] md:pt-[80px]">
-          <h1 className="text-[#36536B] font-display text-[32px] text-center leading-9 md:text-[48px] md:leading-[56px]">
+        <div className="px-6 pt-12 md:px-[100px] md:pt-[80px] lg:px-[165px]">
+          <h1 className="text-[#36536B] font-display text-[32px] text-center leading-9 md:text-[48px] md:leading-[56px] lg:text-[56px]  lg:px-[100px]">
             Submit a help request and we&apos;ll get in touch shortly.
           </h1>
         </div>
-        <div className="mx-6 mt-10 mb-6 md:mx-[162px] md:mt-[54px]">
-          <form className="">
-            <div className='flex flex-col justify-center items-center gap-y-[24px]'>
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="Email Address"
-                className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
-              />
-              <input
-                type="text"
-                name="company-name"
-                placeholder="Company Name"
-                className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
-              />
-              <input
-                type="text"
-                name="title"
-                placeholder="Title"
-                className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
-              />
-              <textarea
-                name="message"
-                placeholder="Message"
-                className="w-full h-[89px] px-[20px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8] resize-none"
-              />
-              <div className="flex justify-center items-center mb-[24px]">
-                <div>
-                  <input type="checkbox" id="subscribe" className="" />
+        <div className="lg:flex lg:justify-center lg:items-center lg:px-[165px] lg:gap-[124px]">
+          <div className="mx-6 mt-10 mb-6 md:mx-[162px] md:mt-[54px] lg:mx-0">
+            <form className="">
+              <div className="flex flex-col justify-center items-center gap-y-[24px]">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
+                />
+                <input
+                  type="text"
+                  name="company-name"
+                  placeholder="Company Name"
+                  className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
+                />
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Title"
+                  className="w-full h-[42px] px-[20px] pb-[17px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8]"
+                />
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  className="w-full h-[89px] px-[20px] border-b-[1px] border-[#36536B]/[.50] bg-[#EDF3F8] resize-none"
+                />
+                <div className="flex justify-center items-center mb-[24px]">
+                  <div>
+                    <input type="checkbox" id="subscribe" className="" />
+                  </div>
+                  <div className="ml-[32px]">
+                    <label
+                      htmlFor="subscribe"
+                      className="text-[#36536B] font-public text-[15px] leading-[25px]"
+                    >
+                      Stay up-to-date with company announcements and updates to
+                      our API
+                    </label>
+                  </div>
                 </div>
-                <div className="ml-[32px]">
-                  <label
-                    htmlFor="subscribe"
-                    className="text-[#36536B] font-public text-[15px] leading-[25px]"
-                  >
-                    Stay up-to-date with company announcements and updates to
-                    our API
-                  </label>
+              </div>
+              <button className="w-[152px] h-[48px] text-[#36536B] border-[#36536B] border-[1px] rounded-3xl mb-12 hover:border-[#36536B] hover:bg-[#36536B] hover:text-[#FFFFFF]">
+                Submit
+              </button>
+            </form>
+          </div>
+          <div className="text-center mx-6">
+            <div>
+              <h3 className="text-[#36536B] font-display text-[24px] leading-8 opacity-75 mb-8 md:mb-10 md:mx-[162px] lg:mx-0">
+                Join the thousands of innovators already building with us
+              </h3>
+              <div className="md:hidden">
+                <div className="flex justify-center items-center">
+                  <Image
+                    src="/assets/shared/desktop/tesla.svg"
+                    alt="payapi-logo"
+                    height={15}
+                    width={107}
+                    className="mr-[25px]"
+                  />
+                  <Image
+                    src="/assets/shared/desktop/microsoft.svg"
+                    alt="payapi-logo"
+                    height={26}
+                    width={120}
+                    className="ml-[25px]"
+                  />
+                </div>
+                <div className="flex justify-center items-center mt-[42px]">
+                  <Image
+                    src="/assets/shared/desktop/hewlett-packard.svg"
+                    alt="payapi-logo"
+                    height={22}
+                    width={115}
+                    className="mr-[28px]"
+                  />
+                  <Image
+                    src="/assets/shared/desktop/oracle.svg"
+                    alt="payapi-logo"
+                    height={14}
+                    width={108}
+                    className="ml-[28px]"
+                  />
+                </div>
+                <div className="flex justify-center items-center mt-[50px]">
+                  <Image
+                    src="/assets/shared/desktop/google.svg"
+                    alt="payapi-logo"
+                    height={22}
+                    width={115}
+                    className="mr-[28px]"
+                  />
+                  <Image
+                    src="/assets/shared/desktop/nvidia.svg"
+                    alt="payapi-logo"
+                    height={22}
+                    width={113}
+                    className="ml-[28px]"
+                  />
                 </div>
               </div>
-            </div>
-            <button className="w-[152px] h-[48px] text-[#36536B] border-[#36536B] border-[1px] rounded-3xl mb-12 hover:border-[#36536B] hover:bg-[#36536B] hover:text-[#FFFFFF]">
-              Submit
-            </button>
-          </form>
-        </div>
-        <div className="text-center mx-6">
-          <div>
-            <h3 className="text-[#36536B] font-display text-[24px] leading-8 opacity-75 mb-8 md:mb-10 md:mx-[162px]">
-              Join the thousands of innovators already building with us
-            </h3>
-            <div className='md:hidden'>
-              <div className="flex justify-center items-center">
-                <Image
-                  src="/assets/shared/desktop/tesla.svg"
-                  alt="payapi-logo"
-                  height={15}
-                  width={107}
-                  className="mr-[25px]"
-                />
-                <Image
-                  src="/assets/shared/desktop/microsoft.svg"
-                  alt="payapi-logo"
-                  height={26}
-                  width={120}
-                  className="ml-[25px]"
-                />
-              </div>
-              <div className="flex justify-center items-center mt-[42px]">
-                <Image
-                  src="/assets/shared/desktop/hewlett-packard.svg"
-                  alt="payapi-logo"
-                  height={22}
-                  width={115}
-                  className="mr-[28px]"
-                />
-                <Image
-                  src="/assets/shared/desktop/oracle.svg"
-                  alt="payapi-logo"
-                  height={14}
-                  width={108}
-                  className="ml-[28px]"
-                />
-              </div>
-              <div className="flex justify-center items-center mt-[50px]">
-                <Image
-                  src="/assets/shared/desktop/google.svg"
-                  alt="payapi-logo"
-                  height={22}
-                  width={115}
-                  className="mr-[28px]"
-                />
-                <Image
-                  src="/assets/shared/desktop/nvidia.svg"
-                  alt="payapi-logo"
-                  height={22}
-                  width={113}
-                  className="ml-[28px]"
-                />
-              </div>
-            </div>
-            <div className="flex-col justify-center items-center hidden md:block">
-              <div className="flex justify-between items-center mx-[119px] mb-[40px]">
-                <Image
-                  src="/assets/shared/desktop/tesla.svg"
-                  alt="payapi-logo"
-                  height={15}
-                  width={107}
-                />
-                <Image
-                  src="/assets/shared/desktop/microsoft.svg"
-                  alt="payapi-logo"
-                  height={26}
-                  width={120}
-                />
-                <Image
-                  src="/assets/shared/desktop/hewlett-packard.svg"
-                  alt="payapi-logo"
-                  height={22}
-                  width={115}
-                />
-              </div>
-              <div className="flex justify-between items-center mx-[119px]">
-                <Image
-                  src="/assets/shared/desktop/oracle.svg"
-                  alt="payapi-logo"
-                  height={14}
-                  width={108}
-                />
-                <Image
-                  src="/assets/shared/desktop/google.svg"
-                  alt="payapi-logo"
-                  height={22}
-                  width={115}
-                />
-                <Image
-                  src="/assets/shared/desktop/nvidia.svg"
-                  alt="payapi-logo"
-                  height={22}
-                  width={113}
-                />
+              <div className="flex-col justify-center items-center hidden md:block">
+                <div className="flex justify-between items-center mx-[119px] mb-[40px] lg:mx-0">
+                  <Image
+                    src="/assets/shared/desktop/tesla.svg"
+                    alt="payapi-logo"
+                    height={15}
+                    width={107}
+                  />
+                  <Image
+                    src="/assets/shared/desktop/microsoft.svg"
+                    alt="payapi-logo"
+                    height={26}
+                    width={120}
+                  />
+                  <Image
+                    src="/assets/shared/desktop/hewlett-packard.svg"
+                    alt="payapi-logo"
+                    height={22}
+                    width={115}
+                  />
+                </div>
+                <div className="flex justify-between items-center mx-[119px] lg:mx-0">
+                  <Image
+                    src="/assets/shared/desktop/oracle.svg"
+                    alt="payapi-logo"
+                    height={14}
+                    width={108}
+                  />
+                  <Image
+                    src="/assets/shared/desktop/google.svg"
+                    alt="payapi-logo"
+                    height={22}
+                    width={115}
+                  />
+                  <Image
+                    src="/assets/shared/desktop/nvidia.svg"
+                    alt="payapi-logo"
+                    height={22}
+                    width={113}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
+
         <div className="mt-20 mb-20">
-          <div>
+          <div className="lg:flex lg:justify-around lg:items-center">
             <h2 className="mb-[32px] text-center text-[#36536B] text-[32px] leading-9 font-display">
               Ready to start?
             </h2>
