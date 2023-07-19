bdee8662707aa4c049e1ac8002c5c07674db586d
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-07-18 18:19:50 -0700

    About Page fully responsive

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,32 +8,34 @@ export default function About() {
     <div className="bg-[#EDF3F8]">
       <div>
         <NavBar />
-        <div className="text-center mx-6 md:mx-10 mt-12 md:mt-[80px]">
-          <h1 className="text-[#36536B] font-display text-[32px] leading-9 mb-10 md:text-[48px] md:leading-[56px] md:mb-[54px]">
-            We empower innovators by delivering access to the financial system
-          </h1>
-          <div className="mb-12 md:mb-[64px] md:flex md:justify-center md:gab-[10px]">
-            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
-              Our Vision
-            </h4>
-            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
-              Our main goal is to build beautiful consumer experiences along
-              with developer-friendly infrastructure. The result is an
-              intelligent tool that gives everyone the ability to create amazing
-              products that solve big problems. We are deeply focused on
-              democratizing financial services through technology.
-            </p>
-          </div>
-          <div className="mb-[76px] md:mb-[92px] md:flex md:justify-center md:gab-[10px]">
-            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
-              Our Business
-            </h4>
-            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
-              At the core of our platform is the technical infrastructure APIs
-              that connect consumers. Our innovative product provides key
-              insights for businesses and individuals, as well as robust
-              reporting for traditional financial institutions and developers.
-            </p>
+        <div className="text-center mx-6 md:mx-10 mt-12 md:mt-[80px] lg:mx-0 lg:mt-[82px]">
+          <div className='lg:mx-[260px]'>
+            <h1 className="text-[#36536B] font-display text-[32px] leading-9 mb-10 md:text-[48px] md:leading-[56px] md:mb-[54px] lg:text-[56px] lg:leading-[56px] lg:w-[730px] lg:mb-[51px]">
+              We empower innovators by delivering access to the financial system
+            </h1>
+            <div className="mb-12 md:mb-[64px] md:flex md:justify-center md:gab-[10px] lg:gap-[30px]">
+              <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px] lg:mb-0 lg:text-left lg:w-[255px]">
+                Our Vision
+              </h4>
+              <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify lg:w-[635px]">
+                Our main goal is to build beautiful consumer experiences along
+                with developer-friendly infrastructure. The result is an
+                intelligent tool that gives everyone the ability to create
+                amazing products that solve big problems. We are deeply focused
+                on democratizing financial services through technology.
+              </p>
+            </div>
+            <div className="mb-[76px] md:mb-[92px] md:flex md:justify-center md:gab-[10px] lg:gap-[30px]">
+              <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px] lg:mb-0 lg:text-left lg:w-[255px]">
+                Our Business
+              </h4>
+              <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify lg:w-[635px]">
+                At the core of our platform is the technical infrastructure APIs
+                that connect consumers. Our innovative product provides key
+                insights for businesses and individuals, as well as robust
+                reporting for traditional financial institutions and developers.
+              </p>
+            </div>
           </div>
         </div>
         <div>
@@ -42,20 +44,27 @@ export default function About() {
             alt="team members"
             width={449}
             height={300}
-            className="md:hidden"
+            className="md:hidden w-full"
           />
           <Image
             src="/assets/about/tablet/image-team-members.jpg"
             alt="team members"
             width={769}
             height={267}
-            className="hidden md:block"
+            className="hidden md:block lg:hidden w-full"
+          />
+          <Image
+            src="/assets/about/desktop/image-team-members.jpg"
+            alt="team members"
+            width={1440}
+            height={500}
+            className="hidden md:hidden lg:block w-full"
           />
         </div>
         <div className="flex justify-center items-center mt-12 md:hidden">
           <div className="bg-[#36536B] w-[327px] h-[1px] mb-4 opacity-25"></div>
         </div>
-        <div className="text-center md:flex md:justify-between md:items-end md:mx-10 md:my-[64px]">
+        <div className="text-center md:flex md:justify-between md:items-end md:mx-10 md:my-[64px] lg:mx-[165px]">
           <div className="mb-8 md:mb-0">
             <div className="bg-[#36536B] w-[223px] h-[1px] mb-4 opacity-25 hidden md:block"></div>
             <h5 className="text-[#36536B] text-[16px] font-public leading-7">
@@ -92,10 +101,10 @@ export default function About() {
         </div>
         <div className="text-center px-6 pb-12">
           <div className="mb-12 md:mb-[64px] md:flex md:justify-center md:gab-[10px]">
-            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
+            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px] lg:mb-0 lg:text-left lg:w-[255px]">
               The Culture
             </h4>
-            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
+            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify lg:w-[635px]">
               We strongly believe there&apos;s always an opportunity to learn
               from each other outside of day-to-day work, whether it&apos;s
               company-wide offsites, internal hackathons, or co-worker meetups.
@@ -103,11 +112,11 @@ export default function About() {
               no matter the job title.
             </p>
           </div>
-          <div className='md:mb-[64px] md:flex md:justify-center md:gab-[10px]'>
-            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px]">
+          <div className="md:mb-[64px] md:flex md:justify-center md:gab-[10px]">
+            <h4 className="text-[#36536B] font-display text-[24px] md:text-[32px] mb-4 leading-8 md:leading-[40px] md:w-[223px] lg:mb-0 lg:text-left lg:w-[255px]">
               The People
             </h4>
-            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify">
+            <p className="text-[#6C8294] font-public text-[15px] leading-7 md:w-[456px] md:text-justify lg:w-[635px]">
               We&apos;re all passionate about building a more efficient and
               inclusive financial infrastructure together. At PayAPI, we have
               diverse backgrounds and skills.
@@ -115,7 +124,7 @@ export default function About() {
           </div>
         </div>
         <div className="mb-20">
-          <div>
+          <div className='lg:flex lg:justify-around lg:items-center'>
             <h2 className="mb-[32px] text-center text-[#36536B] text-[32px] leading-9 font-display">
               Ready to start?
             </h2>
